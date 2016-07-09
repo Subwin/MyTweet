71a380bb3480da78faf6d117c1ee045bc4ef331f
subwin <subwin_zombie@163.com>
2016-07-09 21:51:02 +0800

    新的微博

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import render_template
+from flask import jsonify
+from flask import session
+from flask import abort
+
+
+from models import User
+from models import Tweet
+
+app = Flask(__name__)
+app.secret_key = 'random string'
+
+
+# 通过 session 来获取当前登录的用户
+def current_user():
+    # print('session, debug', session.permanent)
+    username = session.get('username', '')
+    u = User.query.filter_by(username=username).first()
+    return u
+
+
+@app.route('/')
+def index():
+    view = 'login_view'
+    return redirect(url_for(view))
+
+
+# 显示登录界面的函数  GET
+@app.route('/login')
+def login_view():
+    template = 'login.html'
+    return render_template(template)
+
+
+# 处理登录请求  POST
+@app.route('/login', methods=['POST'])
+def login():
+    # u = User(request.form)
+    form = request.get_json()
+    username = form.get('username', '')
+    user = User.query.filter_by(username=username).first()
+    r = {
+        'success': True,
+        'message': '登录成功',
+    }
+    if user is not None and user.validate_auth(form):
+        r['success'] = True
+        r['next'] = url_for('tweet_add_view', username=user.username)
+        session.permanent = True
+        session['username'] = username
+    else:
+        r['success'] = False
+        r['message'] = '登录失败'
+    return jsonify(r)
+
+
+# 处理注册的请求  POST
+@app.route('/register', methods=['POST'])
+def register():
+    form = request.get_json()
+    u = User(form)
+    r = {
+        'success': True,
+        'message': '登录成功',
+    }
+    status, msgs = u.valid()
+    if status:
+        u.save()
+        # r['success'] = True
+        # 下面这句可以在关闭浏览器后保持用户登录
+        session.permanent = True
+        session['username'] = u.username
+        r['next'] = url_for('tweet_add_view', username=u.username)
+    else:
+        r['success'] = False
+        r['message'] = '\n'.join(msgs)
+    return jsonify(r)
+
+
+@app.route('/weibo/add/<username>')
+def tweet_add_view(username):
+    data_u = User.query.filter_by(username=username).first()
+    cur_u = current_user()
+    if data_u is None:
+        abort(404)
+    elif data_u != cur_u:
+        abort(401)
+    else:
+        all_tweets = Tweet.query.all()
+        all_tweets.sort(key=lambda t: t.created_time, reverse=True)
+        return render_template('myTweet.html', user=data_u, all_tweets=all_tweets)
+
+
+@app.route('/tweet/add', methods=['POST'])
+def tweet_add():
+    user = current_user()
+    if user is None:
+        return redirect(url_for('login_view'))
+    else:
+        form = request.get_json()
+        t = Tweet(form)
+        t.user = user
+        t.save()
+        j = t.json()
+        print('j', j)
+        r = {
+            'success': True,
+            'message': '添加成功',
+            'data': t.json
+        }
+        return jsonify(r)
+
+
+@app.route('/tweet/delete/<tweet_id>')
+def tweet_delete(tweet_id):
+    t = Tweet.query.filter_by(id=tweet_id).first()
+    if t is None:
+        abort(404)
+    user = current_user()
+    if user is None or user.id != t.user_id:
+        abort(401)
+    else:
+        t.delete()
+        r = {
+            'success': True,
+            'message': '删除成功',
+        }
+        return jsonify(r)
+
+if __name__ == '__main__':
+    config = {
+        'debug': True,
+    }
+    app.run(**config)
---
+++ models.py
@@ -0,0 +1,158 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import sql
+
+import time
+import shutil
+
+# 数据库的路径
+db_path = './db.sqlite'
+# 获取 app 的实例
+app = Flask(__name__)
+# app = app.app
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.secret_key = 'random string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
+
+db = SQLAlchemy(app)
+
+
+class ReprMixin(object):
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return u'<{}: {}>'.format(class_name, self.id)
+
+
+class Update(object):
+    def update(self, form):
+        new_content = form['content'] if form['content'] != '' else self.content
+        self.content = new_content
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class User(db.Model, ReprMixin, Update):
+    # 类的属性就是数据库表的字段
+    # 这些都是内置的 __tablename__ 是表名
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    created_time = db.Column(db.DateTime(timezone=True), default=sql.func.now())
+    # 这是引用别的表的数据的属性，表明了它关联的东西
+    tweets = db.relationship('Tweet', backref='user')
+
+    def __init__(self, form):
+        super(User, self).__init__()
+        self.username = form.get('username', '')
+        self.password = form.get('password', '')
+
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'created_time': self.created_time,
+            'tweets': self.tweets
+        }
+
+    def validate_auth(self, form):
+        username = form.get('username', '')
+        password = form.get('password', '')
+        username_equals = self.username == username
+        password_equals = self.password == password
+        return username_equals and password_equals
+
+    def update(self, form):
+        print('user.update, ', form)
+        self.password = form.get('password', self.password)
+
+    # 验证注册用户的合法性的
+    def valid(self):
+        valid_username = User.query.filter_by(username=self.username).first() == None
+        valid_username_len = len(self.username) >= 3
+        valid_password_len = len(self.password) >= 3
+        msgs = []
+        if not valid_username:
+            message = '用户名已经存在'
+            msgs.append(message)
+        elif not valid_username_len:
+            message = '用户名长度必须大于等于 6'
+            msgs.append(message)
+        elif not valid_password_len:
+            message = '密码长度必须大于等于 6'
+            msgs.append(message)
+        status = valid_username and valid_username_len and valid_password_len
+        return status, msgs
+
+
+class Tweet(db.Model, ReprMixin, Update):
+    __tablename__ = 'tweets'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String())
+    created_time = db.Column(db.DateTime(timezone=True), default=sql.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref="tweet")
+
+    def __init__(self, form):
+        super(Tweet, self).__init__()
+        self.content = form['content']
+
+    def json(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'created_time': self.created_time,
+            'user_id': self.user_id,
+        }
+
+
+class Comment(db.Model, ReprMixin, Update):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String())
+    created_time = db.Column(db.DateTime(timezone=True), default=sql.func.now())
+    post_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment_tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'))
+
+    def __init__(self, form):
+        super(Comment, self).__init__()
+        self.content = form['content']
+
+    def json(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'created_time': self.created_time,
+            'post_user_id': self.post_user_id,
+            'comment_tweet_id': self.comment_tweet_id,
+        }
+
+
+
+def backup_db():
+    backup_path = '{}.{}'.format(time.time(), db_path)
+    shutil.copyfile(db_path, backup_path)
+
+
+# 定义了数据库，如何创建数据库呢？
+# 调用 db.create_all()
+# 如果数据库文件已经存在了，则啥也不做
+# 所以说我们先 drop_all 删除所有表
+# 再重新 create_all 创建所有表
+def rebuild_db():
+    # backup_db()
+    db.drop_all()
+    db.create_all()
+    print('rebuild database')
+
+
+# 第一次运行工程的时候没有数据库
+# 所以我们运行 models.py 创建一个新的数据库文件
+if __name__ == '__main__':
+    rebuild_db()
