0e8ecf4a8a23f88c6611ec960e94e9150dd9fd95
subwin <subwin_zombie@163.com>
2016-07-10 23:43:42 +0800

    添加功能
    添加我的微博页面

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def login_view():
 # 处理登录请求  POST
 @app.route('/login', methods=['POST'])
 def login():
-    # u = User(request.form)
     form = request.get_json()
     username = form.get('username', '')
     user = User.query.filter_by(username=username).first()
@@ -49,7 +48,7 @@ def login():
     }
     if user is not None and user.validate_auth(form):
         r['success'] = True
-        r['next'] = url_for('tweet_add_view', username=user.username)
+        r['next'] = url_for('timeline', username=user.username)
         session.permanent = True
         session['username'] = username
     else:
@@ -74,15 +73,15 @@ def register():
         # 下面这句可以在关闭浏览器后保持用户登录
         session.permanent = True
         session['username'] = u.username
-        r['next'] = url_for('tweet_add_view', username=u.username)
+        r['next'] = url_for('timeline', username=u.username)
     else:
         r['success'] = False
         r['message'] = '\n'.join(msgs)
     return jsonify(r)
 
 
-@app.route('/tweet/add/<username>')
-def tweet_add_view(username):
+@app.route('/timeline/<username>')
+def timeline(username):
     data_u = User.query.filter_by(username=username).first()
     cur_u = current_user()
     if data_u is None:
@@ -92,12 +91,11 @@ def tweet_add_view(username):
     else:
         all_tweets = Tweet.query.all()
         all_tweets.sort(key=lambda t: t.created_time, reverse=True)
-        # print(all_tweets[0].created_time)
-        return render_template('myTweet.html', user=data_u, all_tweets=all_tweets)
+        return render_template('timeline.html', user=data_u, all_tweets=all_tweets)
 
 
-@app.route('/tweet/add', methods=['POST'])
-def tweet_add():
+@app.route('/timeline', methods=['POST'])
+def timeline_add():
     user = current_user()
     if user is None:
         return redirect(url_for('login_view'))
@@ -112,6 +110,56 @@ def tweet_add():
             'data': t.json()
         }
         return jsonify(r)
+
+
+@app.route('/mytweet/<username>')
+def mytweet_view(username):
+    data_u = User.query.filter_by(username=username).first()
+    cur_u = current_user()
+    if data_u is None:
+        abort(404)
+    elif data_u != cur_u:
+        abort(401)
+    else:
+        t_user = current_user()
+        all_tweets = t_user.tweets
+        # all_tweets = Tweet.query.filter_by(username=username)
+        all_tweets.sort(key=lambda t: t.created_time, reverse=True)
+        return render_template('mytweet.html', user=data_u, all_tweets=all_tweets)
+
+
+@app.route('/mytweet', methods=['POST'])
+def mytweet():
+    user = current_user()
+    if user is None:
+        return redirect(url_for('login_view'))
+    else:
+        form = request.get_json()
+        t = Tweet(form)
+        t.user = user
+        t.save()
+        r = {
+            'success': True,
+            'message': '添加成功',
+            'data': t.json()
+        }
+        return jsonify(r)
+# 两个页面留言板视图函数一样，如何简化？
+
+# @app.route('/tweet/update/<tweet_id>')
+# def tweet_update_view(tweet_id):
+#     one_tweet = Tweet.query.filter_by(id=tweet_id).first()
+#     return render_template('tweet_update.html', tweet=one_tweet, user=current_user())
+#
+#
+# @app.route('/tweet/update/<tweet_id>', methods=['POST'])
+# def todo_update(tweet_id):
+#     form = request.form
+#     one_todo = Tweet.query.filter_by(id=tweet_id).first()
+#     user = one_todo.user
+#     one_todo.update(form)
+#     one_todo.save()
+#     return redirect(url_for('tweet_add_view', username=user.username))
 
 
 @app.route('/tweet/delete/<tweet_id>')
@@ -129,6 +177,7 @@ def tweet_delete(tweet_id):
             'message': '删除成功',
         }
         return jsonify(r)
+
 
 if __name__ == '__main__':
     config = {
